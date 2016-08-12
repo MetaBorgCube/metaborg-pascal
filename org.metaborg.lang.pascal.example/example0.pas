bba414bc139368f001f8b1911a1ea45a254c713e
Eelco Visser <e.visser@tudelft.nl>
2016-08-12 16:44:16 +0200

    examples

--- org.metaborg.lang.pascal.example/example0.pas
+++ org.metaborg.lang.pascal.example/example0.pas
@@ -9,12 +9,35 @@ program Sort(input, output);
         { Type of the element array. }
         IntArrType = array [1..MaxElts] of Integer;
 
-    var
+    var  
         { Indexes, exchange temp, array size. }
         i, j, tmp, size: integer;
         
         { Array of ints }
         arr: IntArrType;
+
+    { Read in the integers. }
+    procedure ReadArr(var size: Integer; var a: IntArrType);
+        begin
+            size := 1;
+            while not eof do begin
+                readln(a[size]);
+                if not eof then
+                  size := size + 1
+            end
+        end;
+
+    { Use quicksort to sort the array of integers. }
+    procedure Quicksort(size: Integer; var arr: IntArrType);
+        { This }
+        procedure QuicksortRecur(start, stop: integer);
+            begin 
+                splitpt := Split(start, stop)
+            end;
+                    
+        begin { Quicksort }
+            QuicksortRecur(1, size)
+        end;
 
     begin
         { Read }
---
+++ org.metaborg.lang.pascal.example/example1.pas
@@ -0,0 +1,6 @@
+program example1();
+  function inc(x : integer) : integer; 
+    begin
+    end;
+begin  
+end. 
